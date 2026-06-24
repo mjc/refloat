b35b46871ca99fb28a8d3db387830d1d9e9fe2c1
Mika Cohen <mjc@kernel.org>
2026-06-24 17:34:46 -0600

    Split circular buffer tests out of pid bundle

--- tests/c_tests.c
+++ tests/c_tests.c
@@ -68,6 +68,7 @@ static bool buffer_item_eq(BufferItem lhs, BufferItem rhs) {
 #include "c_tests/konami_haptic/tests.c"
 #include "c_tests/pid_filters_lcm/tests.c"
 #include "c_tests/lcm/tests.c"
+#include "c_tests/circular_buffer/tests.c"
 
 int main(void) {
     const TestCase tests[] = {        XFAIL_CASE("frequency tracker nonpositive dt", test_frequency_tracker_nonpositive_dt,
--- tests/c_tests/pid_filters_lcm/tests.c
+++ tests/c_tests/pid_filters_lcm/tests.c
@@ -73,28 +73,3 @@ static bool test_sma_allocation_failure_update(void) {
 
     return true;
 }
-
-static bool test_circular_buffer_pop_index(void) {
-    BufferItem storage[3] = {{0}};
-    CircularBuffer cb;
-    circular_buffer_init(&cb, sizeof(BufferItem), 3, storage);
-
-    BufferItem a = {1, 11};
-    BufferItem b = {2, 22};
-    BufferItem c = {3, 33};
-    BufferItem out = {0};
-
-    circular_buffer_push(&cb, &a);
-    circular_buffer_push(&cb, &b);
-    circular_buffer_push(&cb, &c);
-
-    EXPECT_TRUE(circular_buffer_pop(&cb, 1, &out));
-    EXPECT_TRUE(buffer_item_eq(out, b));
-    EXPECT_TRUE(circular_buffer_size(&cb) == 2);
-    EXPECT_TRUE(circular_buffer_get(&cb, 0, &out));
-    EXPECT_TRUE(buffer_item_eq(out, a));
-    EXPECT_TRUE(circular_buffer_get(&cb, 1, &out));
-    EXPECT_TRUE(buffer_item_eq(out, c));
-
-    return true;
-}
