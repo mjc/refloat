328b8820737003c019c793207930adefe44795f6
Mika Cohen <mjc@kernel.org>
2026-07-20 10:13:28 -0600

    Circular buffer: Pop from tail

--- src/lib/circular_buffer.c
+++ src/lib/circular_buffer.c
@@ -74,8 +74,8 @@ bool circular_buffer_get(const CircularBuffer *cb, size_t i, void *item) {
     return true;
 }
 
-bool circular_buffer_pop(CircularBuffer *cb, size_t i, void *item) {
-    if (!circular_buffer_get(cb, i, item)) {
+bool circular_buffer_pop(CircularBuffer *cb, void *item) {
+    if (!circular_buffer_get(cb, 0, item)) {
         return false;
     }
 
